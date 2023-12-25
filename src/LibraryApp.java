dbe7e6b769b305ca0f0b14be2ab5780e939abc3e
EdkUnr <richardlvov1780@gmail.com>
2023-12-25 16:54:09 +1000

    Corrected version homework "String"

--- src/LibraryApp.java
+++ src/LibraryApp.java
@@ -14,46 +14,4 @@ public class LibraryApp {
     }
 }
 
-class Book {
-    private String title;
-    private Author author;
-    private int publicationYear;
 
-    public Book(String title, Author author, int year) {
-        this.title = title;
-        this.author = author;
-        this.publicationYear = year;
-    }
-
-    public void setPublicationYear(int year) {
-        this.publicationYear = year;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Book: %s, Author: %s %s, Year: %d",
-                title, author.getFirstName(), author.getLastName(), publicationYear);
-    }
-}
-
-class Author {
-    private String firstName;
-    private String lastName;
-
-    public Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-}
